311f20108605605d5a9ccc2defa03db2bc3d0fa1
jpolodna <jpolodna707@gmail.com>
2022-05-02 14:43:42 -0500

    init commit

---
+++ app.ts
@@ -0,0 +1,29 @@
+// deno-lint-ignore-file no-explicit-any
+import { Application, Router } from "https://deno.land/x/oak@v10.5.1/mod.ts";
+
+const books = new Map<string, any>();
+books.set("1", {
+  id: "1",
+  title: "The Hound of the Baskervilles",
+  author: "Conan Doyle, Arthur",
+});
+
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body = "Hello world!";
+  })
+  .get("/book", (context) => {
+    context.response.body = Array.from(books.values());
+  })
+  .get("/book/:id", (context) => {
+    if (books.has(context?.params?.id)) {
+      context.response.body = books.get(context.params.id);
+    }
+  });
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
